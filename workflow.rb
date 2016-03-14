37b0d8dbd8c3c6f02f6d420e08ed0c08e8ac92a5
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2016-03-14 10:30:57 +0100

    Initial proyect

---
+++ workflow.rb
@@ -0,0 +1,18 @@
+require 'rbbt-util'
+require 'rbbt/workflow'
+
+Misc.add_libdir if __FILE__ == $0
+
+#require 'rbbt/sources/PCAWG'
+
+module PCAWG
+  extend Workflow
+
+
+end
+
+#require 'PCAWG/tasks/basic.rb'
+
+#require 'rbbt/knowledge_base/PCAWG'
+#require 'rbbt/entity/PCAWG'
+
